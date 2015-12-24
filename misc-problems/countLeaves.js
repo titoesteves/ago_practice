a58d537d34b0375d8601fc4b75d87a519a759088
Tito Esteves <titoesteves@live.com>
2015-12-24 16:51:56 -0500

    ran jscs on file

--- misc-problems/countLeaves.js
+++ misc-problems/countLeaves.js
@@ -1,13 +1,15 @@
-Tree.prototype.countLeaves = function () {
+Tree.prototype.countLeaves = function() {
   var count = 0;
-  function recurse(node){
-    if(node.children.length === 0){
+  function recurse(node) {
+    if (node.children.length === 0) {
       count++;
     }
-    node.children.forEach(function(child){
+
+    node.children.forEach(function(child) {
       recurse(child);
     });
   }
+
   recurse(this);
   return count;
 };
